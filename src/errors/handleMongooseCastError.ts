e9dd4e7ef0568a9b3b0b6783c3bf33eb697aea8c
meherab nahin <nahinrahman87@gmail.com>
2025-01-25 19:51:37 +0600

    mongoose cast error duplicate error

---
+++ src/errors/handleMongooseCastError.ts
@@ -0,0 +1,23 @@
+
+import mongoose from "mongoose";
+import { TErrorSources } from "../errorInterface/errorInterface";
+import { TGenericReturnError } from "../errorInterface/TgenericReturnError";
+
+const handleMongooseCastError = (
+  err: mongoose.Error.CastError
+): TGenericReturnError => {
+  const statusCode = 400;
+  const errorSource: TErrorSources = [
+    {
+      path: err?.path,
+      message: `${err?.message}
+     ${err?.value} is not valid id`,
+    },
+  ];
+  return {
+    statusCode,
+    message: "Invalid Id",
+    errorSource,
+  };
+};
+export default handleMongooseCastError;
